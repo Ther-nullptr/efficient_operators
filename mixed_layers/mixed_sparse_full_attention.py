eed53a9ad0047d997598a9e9eeb715e208e24a84
Ther <1329438302@qq.com>
2024-04-12 01:03:00 +0800

    fix: fix support of pruning

--- mixed_layers/mixed_sparse_full_attention.py
+++ mixed_layers/mixed_sparse_full_attention.py
@@ -46,6 +46,10 @@ class MixedSparseFullAttentionFunc(torch.autograd.Function):
         attention_mask: torch.Tensor,
         use_rotary_pos_enc: bool,
         num_heads: int,
+        small_value_approx: bool,
+        iteration: int,
+        static_value: float,
+        prune_ratio: float,
     ):
         # compute q,k,v
         # forward process: q_proj
@@ -73,6 +77,8 @@ class MixedSparseFullAttentionFunc(torch.autograd.Function):
         q = hidden_to_head_shape(q, num_heads)
         k = hidden_to_head_shape(k, num_heads)
         v = hidden_to_head_shape(v, num_heads)
+        
+        ctx.q_shape = q.shape
 
         # TODO: apply pos_emb to q & k
         if use_rotary_pos_enc:
@@ -103,6 +109,34 @@ class MixedSparseFullAttentionFunc(torch.autograd.Function):
         o_lora_a = o @ w_o_lora_a
         o_lora = o_lora_a @ w_o_lora_b
         o_final = o_main + o_lora
+        
+        if small_value_approx:
+            # save the main weight, then recompute when backward
+            q = (q_main)
+            k = (k_main)
+            v = (v_main)
+        
+        # prune all the values
+        if iteration < 10:
+            kth_val_x = torch.kthvalue(x.abs().flatten(), int(x.numel() * prune_ratio)).values
+            kth_val_q = torch.kthvalue(q.abs().flatten(), int(q.numel() * prune_ratio)).values
+            kth_val_k = torch.kthvalue(k.abs().flatten(), int(k.numel() * prune_ratio)).values
+            kth_val_v = torch.kthvalue(v.abs().flatten(), int(v.numel() * prune_ratio)).values
+            #! notice a is all positive, so compute use a instead of a.abs()
+            kth_val_a = torch.kthvalue(a.flatten(), int(a.numel() * prune_ratio)).values
+            kth_val_o = torch.kthvalue(o.abs().flatten(), int(o.numel() * prune_ratio)).values
+        else:
+            kth_val_x, kth_val_q, kth_val_k, kth_val_v, kth_val_a, kth_val_o = static_value # static_value is a tuple
+
+        ctx.mark_non_differentiable(kth_val_x, kth_val_q, kth_val_k, kth_val_v, kth_val_a, kth_val_o)
+        new_static_value = (kth_val_x, kth_val_q, kth_val_k, kth_val_v, kth_val_a, kth_val_o)
+
+        x = torch.where(x.abs() < kth_val_x, torch.zeros_like(x), x)
+        q = torch.where(q.abs() < kth_val_q, torch.zeros_like(q), q)
+        k = torch.where(k.abs() < kth_val_k, torch.zeros_like(k), k)
+        v = torch.where(v.abs() < kth_val_v, torch.zeros_like(v), v)
+        a = torch.where(a < kth_val_a, torch.zeros_like(a), a)
+        o = torch.where(o.abs() < kth_val_o, torch.zeros_like(o), o)
 
         ctx.save_for_backward(
             x,
@@ -144,14 +178,15 @@ class MixedSparseFullAttentionFunc(torch.autograd.Function):
             w_o_quant_state,
         )
 
-        ctx.q_shape = q.shape
+        
         ctx.a_shape = a.shape
         ctx.q_device = q.device
+        ctx.small_value_approx = small_value_approx
 
-        return o_final
+        return o_final, new_static_value
 
     @staticmethod
-    def backward(ctx, grad_output: torch.Tensor):
+    def backward(ctx, grad_output: torch.Tensor, grad_kth_val):
         (
             x,
             q_lora_a,
@@ -195,6 +230,14 @@ class MixedSparseFullAttentionFunc(torch.autograd.Function):
         
         # reshape
         grad_o = hidden_to_head_shape(grad_o, num_heads)
+        
+        if ctx.small_value_approx:
+            q = q + q_lora_a @ w_q_lora_b
+            k = k + k_lora_a @ w_k_lora_b
+            v = v + v_lora_a @ w_v_lora_b
+            q = hidden_to_head_shape(q, num_heads)
+            k = hidden_to_head_shape(k, num_heads)
+            v = hidden_to_head_shape(v, num_heads)
 
         # backward of second GEMM: O = A @ V
         # d L / d V = A.T @ d L / d O
@@ -217,25 +260,21 @@ class MixedSparseFullAttentionFunc(torch.autograd.Function):
         grad_q = head_to_hidden_shape(grad_q)
         grad_k = head_to_hidden_shape(grad_k)
         grad_v = head_to_hidden_shape(grad_v)
-        
-        q = head_to_hidden_shape(q)
-        k = head_to_hidden_shape(k)
-        v = head_to_hidden_shape(v)
 
         # backward of q_proj
-        grad_w_q_lora_a = q.mT @ (grad_q @ w_q_lora_b.T)
+        grad_w_q_lora_a = x.mT @ (grad_q @ w_q_lora_b.T)
         grad_w_q_lora_b = q_lora_a.mT @ grad_q
         w_q_dequant = F.dequantize_nf4(w_q, w_q_quant_state).to(grad_output.dtype).t()
         grad_x = grad_q @ w_q_dequant.T + grad_q @ w_q_lora_b.T @ w_q_lora_a.T
 
         # backward of k_proj
-        grad_w_k_lora_a = k.mT @ (grad_k @ w_k_lora_b.T)
+        grad_w_k_lora_a = x.mT @ (grad_k @ w_k_lora_b.T)
         grad_w_k_lora_b = k_lora_a.mT @ grad_k
         w_k_dequant = F.dequantize_nf4(w_k, w_k_quant_state).to(grad_output.dtype).t()
         grad_x += grad_k @ w_k_dequant.T + grad_k @ w_k_lora_b.T @ w_k_lora_a.T
 
         # backward of v_proj
-        grad_w_v_lora_a = v.mT @ (grad_v @ w_v_lora_b.T)
+        grad_w_v_lora_a = x.mT @ (grad_v @ w_v_lora_b.T)
         grad_w_v_lora_b = v_lora_a.mT @ grad_v
         w_v_dequant = F.dequantize_nf4(w_v, w_v_quant_state).to(grad_output.dtype).t()
         grad_x += grad_v @ w_v_dequant.T + grad_v @ w_v_lora_b.T @ w_v_lora_a.T
@@ -268,6 +307,10 @@ class MixedSparseFullAttentionFunc(torch.autograd.Function):
             ####################################
             None,
             None,
+            None,
+            None,
+            None,
+            None,
             None
         )
 
@@ -282,6 +325,7 @@ class MixedSparseFullAttention(torch.nn.Module):
         self.hidden_dim = hidden_dim
         self.num_heads = num_heads
         self.iteration = 0
+        self.static_value = None
 
     def forward(
         self,
@@ -300,9 +344,10 @@ class MixedSparseFullAttention(torch.nn.Module):
         o_proj_lora_b: torch.nn.Linear,
         use_rotary_pos_enc: bool,
         attention_mask: torch.Tensor,
+        small_value_approx: bool,
+        prune_ratio: float,
     ):
-        self.iteration += 1
-        return MixedSparseFullAttentionFunc.apply(
+        o_final, static_value = MixedSparseFullAttentionFunc.apply(
             input,
             q_proj_base.weight,
             q_proj_base.bias,
@@ -331,4 +376,19 @@ class MixedSparseFullAttention(torch.nn.Module):
             attention_mask,
             use_rotary_pos_enc,
             self.num_heads,
+            small_value_approx,
+            self.iteration,
+            self.static_value,
+            prune_ratio
         )
+        
+        if self.static_value is None:
+            self.static_value = static_value
+        else:
+            self.static_value = list(self.static_value)
+            for i in range(len(self.static_value)):
+                self.static_value[i] = (self.iteration * self.static_value[i] + static_value[i]) / (self.iteration + 1)
+            self.static_value = tuple(self.static_value)
+        self.iteration += 1
+        
+        return o_final
