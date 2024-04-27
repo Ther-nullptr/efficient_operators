1a2b820c06514de335dfc5c401452db0e36a50a4
Ther <1329438302@qq.com>
2024-04-27 15:50:19 +0800

    chore: add gelu, gemm, hadamard

--- efficient_backward_operators/efficient_gelu.py
+++ efficient_backward_operators/efficient_gelu.py
@@ -1,143 +1,99 @@
 import math
 import torch
 import torch.nn.functional as F
-import bitsandbytes.functional as BF
-from gact.dct_processor import DCTProcessor
-from gact.jpeg_processor import JPEGProcessor
-from gact.memory_efficient_function import (
-    per_block_quantization,
-    per_block_dequantization,
-    dct_compression,
-    jpeg_compression,
-    naive_adjustment,
+from compress_function import (
+    fake_divide_outliner_suboutlinear_svd,
+    get_statistics
 )
-
 
 class EfficientMemoryGELUFunc(torch.autograd.Function):
     @staticmethod
     def forward(
         ctx,
         x,
-        compress_type,
-        jpeg_processor,
-        dct_processor,
-        quantization_shape,
-        prune_ratio,
+        outliner_ratio,
+        sub_outliner_ratio,
+        rank, 
         iteration,
         static_value,
     ):
         result = F.gelu(x)
-        ctx.needs_inputs_grad = x.requires_grad
-        ctx.compress_type = compress_type
-        ctx.quantization_shape = quantization_shape
+        
+        # we just need to use the first batch to calculate the outliner
+        if iteration < 10:
+            outliner, max_norm_column_list, scale = get_statistics(x, iteration, outliner_ratio, sub_outliner_ratio)
+        else:
+            outliner = static_value[0]
+            max_norm_column_list = static_value[1]            
 
-        if compress_type == "NF4":
-            # quantize the cached activation
-            x, quant_state = BF.quantize_nf4(x)
-            ctx.quant_state = quant_state
-        elif compress_type == "PRUNE_ROW":
-            if iteration < 10:
-                kth_val = torch.kthvalue(
-                    x.flatten(), int(x.numel() * prune_ratio)
-                ).values
-            else:
-                kth_val = static_value
-            mask = x > kth_val # when mask is 0, set them to -10, otherwise keep the original value
-            x = (~mask) * -10 + mask * x
-            # x = torch.where(, torch.zeros_like(x) - 10, x)
-        elif compress_type != "NONE":
-            input_shape = x.shape
-            ctx.input_shape = input_shape
-
-            x, quant_state = per_block_quantization(x, input_shape, quantization_shape)
-            ctx.quant_state = quant_state
-
-            if compress_type == "PRUNE":
-                kth_val = torch.kthvalue(x.abs().flatten(), int(x.numel() * 0.1)).values
-                x = torch.where(x.abs() < kth_val, torch.zeros_like(x), x)
-
-            if compress_type == "JPEG":
-                x = jpeg_compression(x, input_shape, jpeg_processor, quantization_shape)
-
-            elif compress_type == "DCT":
-                x = dct_compression(x, input_shape, dct_processor, quantization_shape)
-
-            elif compress_type == "NAIVE":
-                x = naive_adjustment(x, input_shape, quantization_shape)
-        ctx.mark_non_differentiable(kth_val)
+        # inorder to mark save_for_backward, we should convert the tensor
+        max_norm_column_list = torch.tensor(max_norm_column_list)
+            
+        x = fake_divide_outliner_suboutlinear_svd(x, outliner, max_norm_column_list, scale, rank)
+        
+        ctx.mark_non_differentiable(outliner, max_norm_column_list)
         ctx.save_for_backward(x)
-        return result, kth_val
+        
+        return result, outliner, max_norm_column_list
 
     @staticmethod
-    def backward(ctx, grad_output, grad_kth_val):
+    def backward(ctx, grad_output, grad_outliner, grad_max_norm_column_list):
         (x,) = ctx.saved_tensors
-        quantization_shape = ctx.quantization_shape
 
         gamma = math.sqrt(2 / math.pi)
         kappa = 0.044715
         grad_input = None
 
-        if ctx.needs_inputs_grad:
-            if ctx.compress_type == "NF4":
-                x = BF.dequantize_nf4(x, ctx.quant_state)
-            elif ctx.compress_type != "NONE" and ctx.compress_type != "PRUNE_ROW":
-                quant_state = ctx.quant_state
-                input_shape = ctx.input_shape
-                x = per_block_dequantization(
-                    x, input_shape, quant_state, quantization_shape
-                )
-            y = gamma * (x + kappa * x**3)
-            tanh_y = F.tanh(y)
-            grad_input = (
-                0.5
-                * (
-                    (1 + tanh_y)
-                    + x * ((1 - tanh_y**2) * gamma * (1 + 3 * kappa * x**2))
-                )
-                * grad_output
+        y = gamma * (x + kappa * x**3)
+        tanh_y = F.tanh(y)
+        grad_input = (
+            0.5
+            * (
+                (1 + tanh_y)
+                + x * ((1 - tanh_y**2) * gamma * (1 + 3 * kappa * x**2))
             )
+            * grad_output
+        )
 
-        return grad_input, None, None, None, None, None, None, None
+        return grad_input, None, None, None, None, None
 
 
 class EfficientMemoryGELU(torch.nn.Module):
     def __init__(
         self,
-        compress_type: str = "JPEG",
-        compress_quality: int = 50,
-        quantization_shape: int = 64,
-        prune_ratio: float = 0.9,
+        outliner_ratio: float = 0.01,
+        sub_outliner_ratio: float = 0.1, #! initialize
+        rank: int = 16,
     ):
         super(EfficientMemoryGELU, self).__init__()
-        self.compress_type = compress_type
-        self.compress_quality = compress_quality
-        self.jpeg_processor = JPEGProcessor(quality=compress_quality)
-        self.dct_processor = DCTProcessor(
-            quality=compress_quality, interpolation=quantization_shape / 64
-        )
-        self.quantization_shape = quantization_shape
-        self.prune_ratio = prune_ratio
+        self.outliner_ratio = outliner_ratio
+        self.sub_outliner_ratio = sub_outliner_ratio
+        self.rank = rank
         self.iteration = 0
-        self.static_value = None
+        self.static_value = [None, None]
 
     def forward(self, input):
-        result, static_value = EfficientMemoryGELUFunc.apply(
+        result, outliner, max_norm_column_list = EfficientMemoryGELUFunc.apply(
             input,
-            self.compress_type,
-            self.jpeg_processor,
-            self.dct_processor,
-            self.quantization_shape,
-            self.prune_ratio,
+            self.outliner_ratio,
+            self.sub_outliner_ratio,
+            self.rank,
             self.iteration,
             self.static_value,
         )
-        # ema
-        self.static_value = (
-            static_value
-            if self.static_value is None
-            else (self.iteration * self.static_value + static_value)
-            / (self.iteration + 1)
-        )
+
+        if self.iteration <= 10:
+            self.static_value[0] = (
+                outliner
+                if self.static_value[0] is None
+                else (self.iteration * self.static_value[0] + outliner)
+                / (self.iteration + 1)
+            )
+            self.static_value[1] = (
+                max_norm_column_list
+                if self.static_value[1] is None
+                else self.static_value[1]
+            )
         self.iteration += 1
 
         return result
--- efficient_backward_operators/efficient_hadamard.py
+++ efficient_backward_operators/efficient_hadamard.py
@@ -1,14 +1,7 @@
-import math
 import torch
-import bitsandbytes.functional as F
-from gact.dct_processor import DCTProcessor
-from gact.jpeg_processor import JPEGProcessor
-from gact.memory_efficient_function import (
-    per_block_quantization,
-    per_block_dequantization,
-    dct_compression,
-    jpeg_compression,
-    naive_adjustment,
+from compress_function import (
+    fake_divide_outliner_suboutlinear_svd,
+    get_statistics
 )
 
 num_heads, head_dim = 32, 128
@@ -24,229 +17,45 @@ class EfficientMemoryHadamardFunc(torch.autograd.Function):
         ctx,
         x1,
         x2,
-        compress_type,
-        jpeg_processor,
-        dct_processor,
-        quantization_shape,
-        prune_ratio,
+        outliner_ratio_1,
+        sub_outliner_ratio_1,
+        outliner_ratio_2,
+        sub_outliner_ratio_2,
+        rank, 
         iteration,
-        static_value1,
-        static_value2,
+        static_value_1,
+        static_value_2,
     ):
         result = x1 * x2
-        ctx.needs_inputs_grad = [x1.requires_grad, x2.requires_grad]
-        ctx.compress_type = compress_type
-        ctx.quantization_shape = quantization_shape
-
-        kth_val_1 = torch.tensor(0.0, device=x1.device)
-        kth_val_2 = torch.tensor(0.0, device=x2.device)
-        if compress_type == "NF4":
-            # quantize the cached activation
-            x1, quant_state_1 = F.quantize_nf4(x1)
-            x2, quant_state_2 = F.quantize_nf4(x2)
-            ctx.quant_state = quant_state_1, quant_state_2
-        elif compress_type == "PRUNE_ROW":
-            if iteration < 10:
-                kth_val_1 = torch.kthvalue(
-                    x1.flatten(), int(x1.numel() * prune_ratio)
-                ).values
-                kth_val_2 = torch.kthvalue(
-                    x2.flatten(), int(x2.numel() * prune_ratio)
-                ).values
-            else:
-                kth_val_1, kth_val_2 = static_value1, static_value2
-            mask_1 = x1.abs() > kth_val_1
-            x1 = x1 * mask_1
-            mask_2 = x2.abs() > kth_val_2
-            x2 = x2 * mask_2
-        elif compress_type != "NONE":
-            # shape preparation for DCT
-            input_shape = [x1.shape, x2.shape]
-            ctx.input_shape = input_shape
-            # quantize the cached activation
-            bit = 4 if compress_type == "INT4" else 8
-            x1, quant_state1 = per_block_quantization(
-                x1.contiguous(), input_shape[0], quantization_shape, bit=bit
-            )
-            x2, quant_state2 = per_block_quantization(
-                x2.contiguous(), input_shape[1], quantization_shape, bit=bit
-            )
-            ctx.quant_state = [quant_state1, quant_state2]
-
-            if compress_type == "PRUNE":
-                kth_val = torch.kthvalue(
-                    x1.abs().flatten(), int(x1.numel() * 0.75)
-                ).values
-                x1 = torch.where(x1.abs() < kth_val, torch.zeros_like(x1), x1)
-                x1 = naive_adjustment(x1, input_shape[0])
-
-                kth_val = torch.kthvalue(
-                    x2.abs().flatten(), int(x2.numel() * 0.75)
-                ).values
-                x2 = torch.where(x2.abs() < kth_val, torch.zeros_like(x2), x2)
-                x2 = naive_adjustment(x2, input_shape[1])
-
-            # compress the cached activation
-            if compress_type == "JPEG":
-                x1 = jpeg_compression(x1, input_shape[0], jpeg_processor)
-                x2 = jpeg_compression(x2, input_shape[1], jpeg_processor)
-
-            elif compress_type == "DCT":
-                x1 = dct_compression(x1, input_shape[0], dct_processor)
-                x2 = dct_compression(x2, input_shape[1], dct_processor)
-
-            elif compress_type == "NAIVE" or compress_type == "INT4":
-                x1 = naive_adjustment(x1, input_shape[0])
-                x2 = naive_adjustment(x2, input_shape[1])
+        
+        # we just need to use the first batch to calculate the outliner
+        # for the value 1
+        if iteration < 10:
+            outliner_1, max_norm_column_list_1, scale_1 = get_statistics(x1, iteration, outliner_ratio_1, sub_outliner_ratio_1)
+            outliner_2, max_norm_column_list_2, scale_2 = get_statistics(x2, iteration, outliner_ratio_2, sub_outliner_ratio_2)
+        else:
+            outliner_1 = static_value_1[0]
+            max_norm_column_list_1 = static_value_1[1]
+            outliner_2 = static_value_2[0]
+            max_norm_column_list_2 = static_value_2[1]
+            
+        max_norm_column_list_1 = torch.tensor(max_norm_column_list_1)
+        max_norm_column_list_2 = torch.tensor(max_norm_column_list_2)
+        
+        x1 = fake_divide_outliner_suboutlinear_svd(x1, outliner_1, max_norm_column_list_1, scale_1, rank)
+        x2 = fake_divide_outliner_suboutlinear_svd(x2, outliner_2, max_norm_column_list_2, scale_2, rank)
+        
+        ctx.mark_non_differentiable(outliner_1, max_norm_column_list_1, outliner_2, max_norm_column_list_2)
 
         ctx.save_for_backward(x1, x2)
-        ctx.mark_non_differentiable(kth_val_1, kth_val_2)
-        return result, kth_val_1, kth_val_2
-
-    def backward(ctx, grad_output, grad_kth_val1, grad_kth_val2):
+        return result, outliner_1, max_norm_column_list_1, outliner_2, max_norm_column_list_2
+            
+    def backward(ctx, grad_output, grad_outliner_1, grad_max_norm_column_list_1, grad_outliner_2, grad_max_norm_column_list_2):
         x1, x2 = ctx.saved_tensors
-        quantization_shape = ctx.quantization_shape
         grad_input1, grad_input2 = None, None
 
-        if ctx.needs_inputs_grad[0] or ctx.needs_inputs_grad[1]:
-            if ctx.compress_type == "NF4":
-                x1 = F.dequantize_nf4(x1, ctx.quant_state[0])
-                x2 = F.dequantize_nf4(x2, ctx.quant_state[1])
-            elif ctx.compress_type != "NONE" and ctx.compress_type != "PRUNE_ROW":
-                quant_state1, quant_state2 = ctx.quant_state
-                input_shape1, input_shape2 = ctx.input_shape
-                x1 = per_block_dequantization(
-                    x1, input_shape1, quant_state1, quantization_shape
-                )
-                x2 = per_block_dequantization(
-                    x2, input_shape2, quant_state2, quantization_shape
-                )
-
-            if ctx.needs_inputs_grad[0]:
-                grad_input1 = grad_output * x2
-            if ctx.needs_inputs_grad[1]:
-                grad_input2 = grad_output * x1
-
-        return grad_input1, grad_input2, None, None, None, None, None, None, None, None
-
-
-class EfficientMemoryHadamard(torch.nn.Module):
-    def __init__(
-        self,
-        compress_type: str = "JPEG",
-        compress_quality: int = 50,
-        quantization_shape: int = 64,
-        prune_ratio: float = 0.75,
-    ):
-        super().__init__()
-        self.compress_type = compress_type
-        self.compress_quality = compress_quality
-        self.jpeg_processor = JPEGProcessor(quality=compress_quality)
-        self.dct_processor = DCTProcessor(quality=compress_quality)
-        self.quantization_shape = quantization_shape
-        self.prune_ratio = prune_ratio
-        self.iteration = 0
-        self.static_value1 = None
-        self.static_value2 = None
-
-    def forward(self, x1, x2):
-        if self.extract_mode:
-            torch.save(x1, f"output/{self.name}_1.pt")
-            torch.save(x2, f"output/{self.name}_2.pt")
-
-        result, static_value1, static_value2 = EfficientMemoryHadamardFunc.apply(
-            x1,
-            x2,
-            self.compress_type,
-            self.jpeg_processor,
-            self.dct_processor,
-            self.quantization_shape,
-            self.prune_ratio,
-            self.iteration,
-            self.static_value1,
-            self.static_value2,
-        )
-        # ema
-        self.static_value1 = (
-            static_value1
-            if self.static_value1 is None
-            else (self.iteration * self.static_value1 + static_value1)
-            / (self.iteration + 1)
-        )
-        self.static_value2 = (
-            static_value2
-            if self.static_value2 is None
-            else (self.iteration * self.static_value2 + static_value2)
-            / (self.iteration + 1)
-        )
-        self.iteration += 1
-
-        return result
-    
-    
-class EfficientMemoryHadamardFuseLoRAFunc(torch.autograd.Function):
-    @staticmethod
-    def forward(
-        ctx,
-        x1,
-        x2,
-        mask,
-        x1_lora_a,
-        x2_lora_a,
-        w1_lora_b,
-        w2_lora_b,
-        compress_type,
-        prune_ratio,
-        iteration,
-        static_value1,
-        static_value2,
-        batch_size
-    ):
-        # forward: use the original GEMM
-        result = x1 * x2
-        #! just for simulation; in fact, we save x_main directly
-        res1 = (x1_lora_a @ w1_lora_b) * mask
-        res2 = (x2_lora_a @ w2_lora_b)
-        x1_main = x1 # - res1
-        x2_main = x2 - res2
-
-        # prune the x1_main & x2_main
-        kth_val_1 = torch.tensor(0.0, device=x1.device)
-        kth_val_2 = torch.tensor(0.0, device=x2.device)
-        
-        if compress_type == "PRUNE_ROW":
-            if iteration < 10:
-                kth_val_1 = torch.kthvalue(
-                    x1_main.flatten(), int(x1_main.numel() * prune_ratio)
-                ).values
-                kth_val_2 = torch.kthvalue(
-                    x2_main.flatten(), int(x2_main.numel() * prune_ratio)
-                ).values
-            else:
-                kth_val_1, kth_val_2 = static_value1, static_value2
-            mask_1 = x1_main.abs() > kth_val_1
-            x1_main = x1_main * mask_1
-            mask_2 = x2_main.abs() > kth_val_2
-            x2_main = x2_main * mask_2
-
-        ctx.mark_non_differentiable(kth_val_1, kth_val_2)
-        ctx.save_for_backward(
-            x1_main, x2_main, mask, x1_lora_a, x2_lora_a, w1_lora_b, w2_lora_b
-        )
-        ctx.batch_size = batch_size
-        return result, kth_val_1, kth_val_2
-
-    @staticmethod
-    def backward(ctx, grad_out, grad_kth_val_1, grad_kth_val_2):
-        # reconstruct the original x1, x2
-        x1_main, x2_main, mask, x1_lora_a, x2_lora_a, w1_lora_b, w2_lora_b = ctx.saved_tensors
-        
-        res1 = (x1_lora_a @ w1_lora_b) * mask
-        res2 = (x2_lora_a @ w2_lora_b)
-        x1 = x1_main # + res1
-        x2 = x2_main + res2
-        grad_input1 = grad_out * x2
-        grad_input2 = grad_out * x1
+        grad_input1 = grad_output * x2
+        grad_input2 = grad_output * x1
 
         return (
             grad_input1,
@@ -259,54 +68,67 @@ class EfficientMemoryHadamardFuseLoRAFunc(torch.autograd.Function):
             None,
             None,
             None,
-            None,
-            None,
-            None
         )
 
 
-class EfficientMemoryHadamardFuseLoRA(torch.nn.Module):
+class EfficientMemoryHadamard(torch.nn.Module):
     def __init__(
         self,
-        compress_type: str = "PRUNE_ROW",
-        prune_ratio: float = 0.90,
+        outliner_ratio_1: float = 0.01,
+        sub_outliner_ratio_1: float = 0.1,
+        outliner_ratio_2: float = 0.01,
+        sub_outliner_ratio_2: float = 0.1,
+        rank: int = 16,
     ):
-        super().__init__()
-        self.compress_type = compress_type
-        self.prune_ratio = prune_ratio
+        super(EfficientMemoryHadamard).__init__()
+        self.outliner_ratio_1 = outliner_ratio_1
+        self.sub_outliner_ratio_1 = sub_outliner_ratio_1
+        self.outliner_ratio_2 = outliner_ratio_2
+        self.sub_outliner_ratio_2 = sub_outliner_ratio_2
+        self.rank = rank
         self.iteration = 0
-        self.static_value1 = None
-        self.static_value2 = None
-        
-    def forward(self, x1, x2, mask, x1_lora_a, x2_lora_a, w1_lora_b, w2_lora_b, batch_size):
-        result, static_value1, static_value2 = EfficientMemoryHadamardFuseLoRAFunc.apply(
+        self.static_value_1 = [None, None]
+        self.static_value_2 = [None, None]
+
+    def forward(self, x1, x2):
+        result, outliner_1, max_norm_column_list_1, outliner_2, max_norm_column_list_2 = EfficientMemoryHadamardFunc.apply(
             x1,
             x2,
-            mask,
-            x1_lora_a,
-            x2_lora_a,
-            w1_lora_b,
-            w2_lora_b,
-            self.compress_type,
-            self.prune_ratio,
+            self.outliner_ratio_1,
+            self.sub_outliner_ratio_1,
+            self.outliner_ratio_2,
+            self.sub_outliner_ratio_2,
+            self.rank,
             self.iteration,
-            self.static_value1,
-            self.static_value2,
-            batch_size
+            self.static_value_1,
+            self.static_value_2,
         )
-        
-        self.static_value1 = (
-            static_value1
-            if self.static_value1 is None
-            else (self.iteration * self.static_value1 + static_value1)
-            / (self.iteration + 1)
-        )
-        self.static_value2 = (
-            static_value2
-            if self.static_value2 is None
-            else (self.iteration * self.static_value2 + static_value2)
-            / (self.iteration + 1)
-        )
+        if self.iteration <= 10:
+            self.static_value_1[0] = (
+                outliner_1
+                if self.static_value_1[0] is None
+                else (self.iteration * self.static_value_1[0] + outliner_1)
+                / (self.iteration + 1)
+            )
+            self.static_value_1[1] = (
+                max_norm_column_list_1
+                if self.static_value_1[1] is None
+                else (self.iteration * self.static_value_1[1] + max_norm_column_list_1)
+                / (self.iteration + 1)
+            )
+            self.static_value_2[0] = (
+                outliner_2
+                if self.static_value_2[0] is None
+                else (self.iteration * self.static_value_2[0] + outliner_2)
+                / (self.iteration + 1)
+            )
+            self.static_value_2[1] = (
+                max_norm_column_list_2
+                if self.static_value_2[1] is None
+                else (self.iteration * self.static_value_2[1] + max_norm_column_list_2)
+                / (self.iteration + 1)
+            )
         self.iteration += 1
-        
+
         return result
+
